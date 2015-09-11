9cb6604efb5d3d38e4ba4162b113299eb16113cd
AlexKbit <sav4enkoaleksandr@gmail.com>
2015-09-11 20:53:57 +0400

    Initialize project CCompiler
    Initialize project CCompiler

---
+++ CCompiler/CCompiler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CCompiler
+{
+    class CCompiler
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I'm CCompiler!");
+        }
+    }
+}
